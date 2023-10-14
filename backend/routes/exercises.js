b5fdfb4f8057e3d179384474480945ae2b6bb252
harshkiprofile <beer18317@gmail.com>
2023-10-14 16:55:32 +0000

    Routing started

---
+++ backend/routes/exercises.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const Exercise = require('../models/exercise.model');
+
+
+router.route('/').get( (req, res) => {
+    Exercise.find()
+        .then(exercises => res.json(exercises))
+        .catch(err => res.status(400).json('Error:' + err))
+} )
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -33,6 +33,12 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json())
 
+const exercisesRouter = require('./routes/exercises');
+const usersRouter     = require('./routes/users')
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
